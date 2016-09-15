ce0dc316d941cf68828bdd584bbfcbf62bf20554
jayakrishnan <jayakrishnancn@gmail.com>
2016-09-15 13:14:38 +0530

    initial commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,122 +1,187 @@
-var gulp=require("gulp"),
-	jade=require('gulp-jade'),
-	sass=require('gulp-sass'),
-	plumber = require('gulp-plumber'),
-	rename =require('gulp-rename'),
-	browserSync=require('browser-sync'),
-	zip=require('gulp-zip'),
-	autoprefixer = require('gulp-autoprefixer');
+var gulp =require('gulp'),
+    browserSync=require('browser-sync'),
+    jade=require('gulp-jade'),
+    autoprefixer=require('gulp-autoprefixer'),
+    rename=require('gulp-rename'),
+    sass=require('gulp-sass'),
+    imagemin = require('gulp-imagemin'),
+    plumber = require('gulp-plumber'),
+    htmlmin = require('gulp-html-minifier'),
+    uglify = require('gulp-uglify'),
+    prettify = require('gulp-jsbeautifier');
 
-var	uglify = require('gulp-uglify'),
-	imagemin = require('gulp-imagemin'),
-	htmlmin = require('gulp-html-minifier'),
-	cleanCSS = require('gulp-clean-css');
+/* ------------------------ Directory Settings ---------------------- */
+var devPath  = './dev',
+    jadePath = devPath + "/jade",
+    sassPath = devPath + "/sass",
+    devCssPath  = devPath + "/css",
+    devJsPath  = devPath + "/js",
+    devImgPath  = devPath + "/images"
+    devVendorPath  = devPath + "/vendor";
 
-var prettify = require('gulp-jsbeautifier');
-/*
-*  directories
-*/
-var app="app";
-var htmlPath="dest/public";
-var assets=htmlPath+"/assets";
-var sassPath=app+"/sass";
-var jadePath=app+"/jade";
-var jsPath=app+"/js";
-var cssPath=assets+"/css";
-var imgPath=app+"/images";
-var portNumber=8011;
-/* tasks */
-/*
-* 	server
-*/
+var destPath = "./dest",
+    htmlPath = destPath + "/site",
+    cssPath = htmlPath  +"/assets/css",
+    imgPath = htmlPath  +"/assets/images",
+    jsPath = htmlPath  +"/assets/js";
+    vendorPath = htmlPath  +"/assets/vendor";
 
-/* jade task */
-gulp.task('jade',function(){
-	gulp.src([jadePath+"/**/*.jade",'!'+jadePath+'/**/_*/**/*'])
-	.pipe(plumber())
-	.pipe(jade({
+
+
+/* ------------------------ task config Settings ---------------------- */
+
+var  portNumber= 8001; /* port for browserSync UI */
+
+var sassSettings={
+      outputStyle: 'expanded',
+      onError: browserSync.notify
+    };
+
+var jadeSettings={
       pretty: true
-    }))
-   /* .pipe(htmlmin({collapseWhitespace: true,collapseBooleanAttributes: true,minifyCSS: true,minifyJS: true,minifyURLs: true,removeAttributeQuotes: true,removeComments:true,removeEmptyAttributes:true}))*/
-    .pipe(gulp.dest(htmlPath))
-});
-/* jade task */
-gulp.task('zipit',function(){
-	gulp.src([htmlPath+"/**/*",'!'+htmlPath+'/**/_*/**/*'])
-  .pipe(zip('public.zip'))
-  .pipe(gulp.dest('dest'));
+    };
+
+var autoprefixerSettings={
+      browsers: ['last 35 versions'],
+      cascade: false
+    };
+
+var browserSyncSettings={
+      server: {
+        baseDir: htmlPath
+      },
+      ui: {
+        port: portNumber
+      }
+    };
+var uglifySettings={
+      sequences: true,
+      dead_code: true,
+      drop_debugger: true,
+      conditionals: true,
+      booleans: true,
+      loops: true,
+      evaluate: true,
+      unused: true,
+      if_return: true,
+      join_vars: true,
+      cascade: true,
+      drop_console: true
+    };
+var htmlSettings={
+      collapseWhitespace: true,
+      collapseBooleanAttributes: true,
+      minifyCSS: true,
+      minifyJS: true,
+      minifyURLs: true,
+      removeAttributeQuotes: true,
+      removeComments:true,
+      removeEmptyAttributes:true
+    };
+var plumberSettings=function(error){
+  console.log(error);
+  this.emit('end');
+}
+/* ------------------------ Exprn match ---------------------- */
+
+var htmlmatch=[
+                htmlPath +"/*.html"
+              ],
+    jadematch=[
+                jadePath  + "/**/*.jade" ,
+                '!' + jadePath  + '/**/_*/**/*'
+              ],
+    sassmatch=[
+                sassPath  + "/**/*.sass",
+                sassPath  + "/**/*.scss",
+                '!' +  sassPath  + '/**/_*/**/*'
+              ],
+    sassallmatch=[
+                sassPath  + "/**/*.sass",
+                sassPath  + "/**/*.scss" 
+              ],
+    jsmatch=[
+              devJsPath+"/**/*.js",
+              "!"+devJsPath+"/**/*.min.js",
+              '!' + devJsPath  + '/**/_*/**/*'
+            ],
+    imgmatch=[devImgPath+'/**/*'],
+    vendormatch=[devVendorPath+'/**/*'],
+    jadeallmatch=[
+                jadePath  + "/**/*.jade"
+              ];
+
+/* ------------------------ Vendor Copy  ---------------------- */
+gulp.task('vendor',function(){
+  return gulp.src(vendormatch)
+        .pipe(plumber(plumberSettings))
+        .pipe(gulp.dest(vendorPath))
 });
 
-
-/* sass task */
+/* ------------------------ sass  task ---------------------- */
 gulp.task('sass',function(){
-	gulp.src([sassPath+"/**/*.sass",sassPath+"/**/*.scss"])
-	.pipe(plumber())
-	.pipe(sass({outputStyle: 'expanded'}).on('error',function(x){console.log(x);},console.log))
-    .pipe(autoprefixer({
-            browsers: ['last 35 versions'],
-            cascade: false
-        }))
-    /*.pipe(cleanCSS({compatibility: 'ie8'}))*/
-	.pipe(gulp.dest(cssPath))
-	.pipe(browserSync.stream());
+  return gulp.src(sassmatch)
+        .pipe(plumber(plumberSettings))
+        .pipe(sass(sassSettings)).on('error',function(e){console.log(e);})
+        .pipe(autoprefixer(autoprefixerSettings))
+        .pipe(gulp.dest(cssPath))
+        .pipe(browserSync.reload({stream:true}))
+        .pipe(gulp.dest(devCssPath))
 });
 
+/*gulp.task('jadetest',function(){
+  return gulp.src(jadeallmatch)
+        .pipe(jade(jadeSettings)) 
+});*/
+ 
+/* ------------------------ jade  task ---------------------- */
+gulp.task('jade',function(){
+ 
+  return gulp.src(jadematch)
+        .pipe(plumber(plumberSettings))
+        .pipe(jade(jadeSettings))
+        /*.pipe(htmlmin(htmlSettings))*/
+        .pipe(gulp.dest(htmlPath)) 
 
+});
+ 
 
-/* sass task */
+/* ------------------------ js  task ---------------------- */
 gulp.task('scripts',function(){
-	gulp.src([jsPath+"/**/*.js","!"+jsPath+"/**/*.min.js"])
-	.pipe(plumber())
-	.pipe(rename({suffix:'.min'}))
-	/*.pipe(uglify({
-		sequences: true,
-		dead_code: true,
-		drop_debugger: true,
-		conditionals: true,
-		booleans: true,
-		loops: true,
-		evaluate: true,
-		unused: true,
-		if_return: true,
-		join_vars: true,
-		cascade: true,
-		drop_console: true
-	}))*/
-    .pipe(prettify())
-	.pipe(gulp.dest(assets+'/js'));
+  return gulp.src(jsmatch) 
+        .pipe(plumber(plumberSettings))
+        .pipe(rename({suffix:'.min'}))
+        /*.pipe(uglify(uglifySettings))*/
+        .pipe(prettify())
+        .pipe(gulp.dest(jsPath))
 });
 
-gulp.task('imgmin',() =>
 
-gulp.src(imgPath+'/**/*')
-		.pipe(plumber())
+/* ------------------------ image min  task ---------------------- */
+gulp.task('imgmin',function(){
+  return gulp.src(imgmatch)
+        .pipe(plumber(plumberSettings))
         .pipe(imagemin())
-        .pipe(gulp.dest(assets+"/images"))
-);
-//
-//	watch
-//
+        .pipe(gulp.dest(imgPath))
+});
+
+
+/* ------------------------ browserSync  task ---------------------- */
+gulp.task('browser-sync', ['sass', 'jade','scripts'], function() {
+  return browserSync(browserSyncSettings);
+});
+
+
+/* ------------------------  watch  task -------------------------- */
 gulp.task('watch',function(){
-	browserSync.init({
-	    server: {
-	        baseDir: htmlPath,
-	        directories: true
-	    },
-	    ui: {
-			port: portNumber
-		}
-	});
+  gulp.watch(sassallmatch,['sass']);
+  gulp.watch(imgmatch,['imgmin']);
+  gulp.watch(jsmatch,['scripts']);
+  gulp.watch(jadeallmatch,['jade']);
+  gulp.watch(htmlmatch).on('change',browserSync.reload);
+
+});
 
 
-	gulp.watch(jadePath+"/**/*.jade",['jade']);
-	gulp.watch([sassPath+"/**/*.sass",sassPath+"/**/*.scss"],['sass']);
-	gulp.watch(jsPath+"/**/*.js",['scripts']);
-	gulp.watch(imgPath+"/**/*",['imgmin']);
-	gulp.watch([htmlPath+"/*.html"]).on('change',browserSync.reload);
-	});
-
-/*
-*  default task
-*/
-gulp.task('default',[ 'imgmin','sass','scripts','jade','watch']);
+/* ------------------------ default gulp task -------------------------- */
+gulp.task('default',['vendor','browser-sync','watch']);
